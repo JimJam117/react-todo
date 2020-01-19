a7ede66c16fe0f4a458ecfa2de6cbf72a8912bfb
jimjam117 <jamessparrow101@gmail.com>
2020-01-19 13:07:21 +0000

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+
+function App() {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <div>
+                        <input type="checkbox" name="" id=""/>
+                        <span>Item 1</span> 
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <input type="checkbox" name="" id=""/>
+                        <span>Item 2</span> 
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <input type="checkbox" name="" id=""/>
+                        <span>Item 3</span> 
+                    </div>
+                </li>
+
+            </ul>
+
+        </div>
+    );
+
+}
+
+export default App;
